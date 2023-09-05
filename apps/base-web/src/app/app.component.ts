5e9760c6bedaa5e4ab374b94b12386f5837fd5a5
Jayesh Tajane <jayeshtajane@gmail.com>
2023-09-06 00:11:22 +0530

    Initial commit

---
+++ apps/base-web/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'biz-assist-ui-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass'],
+})
+export class AppComponent {
+  title = 'base-web';
+}
